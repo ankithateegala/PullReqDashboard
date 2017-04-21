b8e618821988ecae3a39054a8221a4238c1286e2
ankitha <ankitha.rao.t@gmail.com>
2017-04-21 00:13:30 -0500

    m

---
+++ PullReqDashboard.API/src/PullReqDashboard.API/Models/ServiceHooksEvent/PullRequestUpdated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PullReqDashboard.API.Models.ServiceHooksEvent
+{
+    public class PullRequestUpdated
+    {
+        public Guid id { get; set; }
+        public string eventType { get; set; }
+        public IEnumerable<Reviewer> reviewers { get; set; }
+
+    }
+
+    public class Reviewer
+    {
+        public string displayName { get; set; }
+        public int vote { get; set; }
+    }
+}

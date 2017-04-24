55d1373f054ad93fa59b55173f832a469418caa1
ankitha <ankitha.rao.t@gmail.com>
2017-04-23 19:33:19 -0500

    m

--- PullReqDashboard.API/src/PullReqDashboard.API/Utilities/DBHelper.cs
+++ PullReqDashboard.API/src/PullReqDashboard.API/Utilities/DBHelper.cs
@@ -17,7 +17,7 @@ namespace PullReqDashboard.API.Utilities
         private string connectionString;
         public DBHelper()
         {
-            connectionString = @"Server=witomyf-ohio.cdqgdsizulw9.us-east-2.rds.amazonaws.com,1433;Database=PullRequestDashboard-Dev;User Id =admin;Password=adminwitomyfohio;";
+            connectionString = @"Server=witomyf-ohio.cdqgdsizulw9.us-east-2.rds.amazonaws.com,1433;Database=PullRequestDashboard-Dev;";
         }
         private IDbConnection Connection
         {
